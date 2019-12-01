bf60077c02404f8bc14cd9a124dd27e39f406e3c
unknown <you@example.com>
2019-12-01 22:04:55 +0800

    another commit

---
+++ Work/No.8/js/new_file.js
@@ -0,0 +1,29 @@
+var count = 0;
+
+function add() {
+	var a = document.createElement("tr");
+	var tab = document.getElementById("tab");
+	var name = document.getElementById("newname").value;
+	var num = document.getElementById("num").value;
+	tab.innerHTML +=
+		"<tr id='" + count + "'>" +
+		"<td>" + name + "</td>" +
+		"<td>" + num + "</td>" +
+		"<td><a href='javascript:delet(" + count + ")'>" +
+		//"<td><a onclick='delet("+count+")'>"+
+		"Delete" +
+		"</a></td>" +
+		"</tr>";
+	count++;
+}
+
+function delet(count) {
+	var row = document.getElementById(count);
+	row.remove(count);
+}
+
+function empty() {
+	var all;
+	all = document.getElementById(count);
+	all.remove(count);
+}
